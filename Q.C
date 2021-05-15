f53d9591cb5fcd5aa96322e6f21f8fda23861a54
iiillhyhl <63485006+iiillhyhl@users.noreply.github.com>
2021-05-15 15:02:44 +0900

    Create Q.C

---
+++ Q.C
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+int main()
+{
+	char a[50];
+	int n;
+	int t=0;
+	
+	scanf("%d\n",&n);
+	
+	
+	scanf("%s",a);
+	
+ for(int j=0; j<n; j++)
+	{
+		if(a[j] == "S")
+		{
+			if(a[j+1] = = "J")
+			t = t+1;	
+		}
+	
+	}
+	printf("%d",t);
+}
+
+	
